903c7d3444d8061a747c0eaed9e0b8ede4078294
chrismachado <christianomachado10@gmail.com>
2017-12-11 00:29:23 -0200

    Update Main.java

--- src/main/Main.java
+++ src/main/Main.java
@@ -9,7 +9,7 @@ import search.AgentUCS;
 public class Main {
     public static void main(String[] args) {
 
-        RomaniaMap romania = new RomeniaMap();
+        RomaniaMap romania = new RomaniaMap();
 
         AbstractAgent agent = new AgentUCS(romenia.oradea(),romenia.craiova());
 
