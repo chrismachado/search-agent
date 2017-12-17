014a8fcf46f933d0cd372fb21b5415c6bf0f5059
Unknown <christianomachado10@gmail.com>
2017-12-16 22:38:38 -0300

    Código dos agentes BFS e UCS mais limpos.

--- src/search/AgentUCS.java
+++ src/search/AgentUCS.java
@@ -7,38 +7,31 @@ import java.util.*;
 
 public class AgentUCS extends AgentBFS implements Comparator<Node> {
 
-    private Node source, goals;
     private PriorityQueue<Node> queue;
-    private Set<Node> explored;
-    private boolean found;
 
     public AgentUCS(Node source, Node goals) {
-        super();
-        this.source = source;
-        this.goals = goals;
-        this.source.setPathCost(0);
+        super(source, goals, new HashSet<>(), false);
+        super.getSource().setPathCost(0);
         this.queue = new PriorityQueue<Node>(20,this::compare);
-        this.explored = new HashSet<Node>();
-        this.found = false;
     }
 
     @Override
     public void search() {
-        this.queue.add(source);
+        this.queue.add(super.getSource());
         Node current;
         do {
             current = queue.poll();
-            explored.add(current);
+            super.getExplored().add(current);
 
-            if(current.getValue().equals(goals.getValue()))
-                found = true;
+            if(current.getValue().equals(super.getGoals().getValue()))
+                super.setFound(true);
 
             for (Edge e : current.getAdjacencies()) {
                 Node child = e.getTarget();
                 int cost = e.getCost();
                 child.setPathCost(current.getPathCost() + cost);
 
-                if(!explored.contains(child) && !queue.contains(child)) {
+                if(!super.getExplored().contains(child) && !queue.contains(child)) {
                     child.setParent(current);
                     queue.add(child);
                 } else if(queue.contains(child) &&
@@ -51,10 +44,10 @@ public class AgentUCS extends AgentBFS implements Comparator<Node> {
 
             }
 
-        } while(!queue.isEmpty());
+        } while(!queue.isEmpty() && !super.isFound());
 
         System.out.println("=============     PATH     =============");
-        System.out.println(printPath(goals));
+        System.out.println(super.printPath(super.getGoals()));
         System.out.println("========================================");
     }
 
@@ -65,17 +58,5 @@ public class AgentUCS extends AgentBFS implements Comparator<Node> {
         if (n1.getPathCost() < n2.getPathCost()) return -1;
 
         return 0;
-    }
-
-    @Override
-    public List<Node> printPath(Node target) {
-        List<Node> path = new ArrayList<Node>();
-
-        for(Node node = target; node != null; node = node.getParent())
-            path.add(node);
-
-        Collections.reverse(path);
-
-        return path;
     }
 }
